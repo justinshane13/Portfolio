57f38652649fea23ee762e9827b1aa6ec8193b83
justinshane13 <justinshane13@gmail.com>
2022-06-28 12:46:47 -0700

    Restructured site to be on one page

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// Default of page is dark mode
 let lightMode = false
 
+// Function to toggle between light and dark mode
 document.getElementById("mode-button").addEventListener("click", toggleMode)
 
+// Function to handle all css changes between light and dark mode
 function toggleMode() {
     if (lightMode === false) {
         lightMode = true
         document.body.style.backgroundColor = "rgb(240, 238, 244)"
         document.body.style.text = "rgb(240, 238, 244)"
+        document.getElementById("navbar").style.backgroundColor = "rgb(240, 238, 244)"
         document.getElementById("slider-round").textContent = "Light"
         document.getElementById("slider-round").style.textAlign = "left"
         document.getElementById("slider-round").style.color = "rgb(240, 238, 244)"
         document.getElementById("slider-round").textContent = "Light"
         document.getElementById("home-button").style.color = "rgb(31, 32, 40)"
-        document.getElementById("title").style.color = "rgb(31, 32, 40)"
-        document.getElementById("role").style.color = "rgb(31, 32, 40)"
         document.getElementById("projects-button").style.color = "rgb(31, 32, 40)"
         document.getElementById("about-button").style.color = "rgb(31, 32, 40)"
+        document.getElementById("contact-button").style.color = "rgb(31, 32, 40)"
+        document.getElementById("title").style.color = "rgb(31, 32, 40)"
+        document.getElementById("role").style.color = "rgb(31, 32, 40)"
         document.getElementById("summary").style.color = "rgb(31, 32, 40)"
         document.getElementById("summary2").style.color = "rgb(31, 32, 40)"
         document.getElementById("linkedin-text").style.color = "rgb(31, 32, 40)"
@@ -32,14 +37,16 @@ function toggleMode() {
         lightMode = false
         document.body.style.backgroundColor = "rgb(31, 32, 40)"
         document.body.style.text = "rgb(31, 32, 40)"
+        document.getElementById("navbar").style.backgroundColor = "rgb(31, 32, 40)"
         document.getElementById("slider-round").textContent = "Dark"
         document.getElementById("slider-round").style.textAlign = "right"
         document.getElementById("slider-round").style.color = "rgb(31, 32, 40)"
         document.getElementById("home-button").style.color = "rgb(240, 238, 244)"
-        document.getElementById("title").style.color = "rgb(240, 238, 244)"
-        document.getElementById("role").style.color = "rgb(240, 238, 244)"
         document.getElementById("projects-button").style.color = "rgb(240, 238, 244)"
         document.getElementById("about-button").style.color = "rgb(240, 238, 244)"
+        document.getElementById("contact-button").style.color = "rgb(240, 238, 244)"
+        document.getElementById("title").style.color = "rgb(240, 238, 244)"
+        document.getElementById("role").style.color = "rgb(240, 238, 244)"
         document.getElementById("summary").style.color = "rgb(240, 238, 244)"
         document.getElementById("summary2").style.color = "rgb(240, 238, 244)"
         document.getElementById("linkedin-text").style.color = "rgb(240, 238, 244)"
@@ -53,7 +60,6 @@ function toggleMode() {
         document.getElementById("github-image").src = "./images/GitHub-Mark-Light-120px-plus.png"
         document.getElementById("email-image").src = "./images/mail.svg"
     }
-    console.log(lightMode)
 }
 
 // Changes for project box #1
@@ -63,7 +69,6 @@ document.getElementById("img-container1").addEventListener("mouseout", hideButto
 function showButtons1() {
     document.getElementById(`img-button-a`).style.opacity = "1"
     document.getElementById(`img-button-b`).style.opacity = "1"
-    document.getElementById(`img-button-c`).style.opacity = "1"
     document.getElementById(`project-img1`).style.opacity = "0.15"
     document.getElementById(`made-with1`).style.opacity = "1"
 }
@@ -71,7 +76,6 @@ function showButtons1() {
 function hideButtons1() {
     document.getElementById("img-button-a").style.opacity = "0"
     document.getElementById("img-button-b").style.opacity = "0"
-    document.getElementById("img-button-c").style.opacity = "0"
     document.getElementById("project-img1").style.opacity = "1"
     document.getElementById(`made-with1`).style.opacity = "0"
 }
@@ -83,7 +87,6 @@ document.getElementById("img-container2").addEventListener("mouseout", hideButto
 function showButtons2() {
     document.getElementById(`img-button-d`).style.opacity = "1"
     document.getElementById(`img-button-e`).style.opacity = "1"
-    document.getElementById(`img-button-f`).style.opacity = "1"
     document.getElementById(`project-img2`).style.opacity = "0.15"
     document.getElementById(`made-with2`).style.opacity = "1"
 }
@@ -91,7 +94,6 @@ function showButtons2() {
 function hideButtons2() {
     document.getElementById("img-button-d").style.opacity = "0"
     document.getElementById("img-button-e").style.opacity = "0"
-    document.getElementById("img-button-f").style.opacity = "0"
     document.getElementById("project-img2").style.opacity = "1"
     document.getElementById(`made-with2`).style.opacity = "0"
 }
@@ -103,7 +105,6 @@ document.getElementById("img-container3").addEventListener("mouseout", hideButto
 function showButtons3() {
     document.getElementById(`img-button-g`).style.opacity = "1"
     document.getElementById(`img-button-h`).style.opacity = "1"
-    document.getElementById(`img-button-i`).style.opacity = "1"
     document.getElementById(`project-img3`).style.opacity = "0.15"
     document.getElementById(`made-with3`).style.opacity = "1"
 }
@@ -111,7 +112,6 @@ function showButtons3() {
 function hideButtons3() {
     document.getElementById("img-button-g").style.opacity = "0"
     document.getElementById("img-button-h").style.opacity = "0"
-    document.getElementById("img-button-i").style.opacity = "0"
     document.getElementById("project-img3").style.opacity = "1"
     document.getElementById(`made-with3`).style.opacity = "0"
 
@@ -124,7 +124,6 @@ document.getElementById("img-container4").addEventListener("mouseout", hideButto
 function showButtons4() {
     document.getElementById(`img-button-j`).style.opacity = "1"
     document.getElementById(`img-button-k`).style.opacity = "1"
-    document.getElementById(`img-button-l`).style.opacity = "1"
     document.getElementById(`project-img4`).style.opacity = "0.15"
     document.getElementById(`made-with4`).style.opacity = "1"
 }
@@ -132,7 +131,6 @@ function showButtons4() {
 function hideButtons4() {
     document.getElementById("img-button-j").style.opacity = "0"
     document.getElementById("img-button-k").style.opacity = "0"
-    document.getElementById("img-button-l").style.opacity = "0"
     document.getElementById("project-img4").style.opacity = "1"
     document.getElementById(`made-with4`).style.opacity = "0"
 }
